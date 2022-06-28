ffb279e136a363cd85bb5f1056d7feaea287ff88
Mesto <xamlol2982002@gmail.com>
2022-06-28 16:56:59 +0700

    built default layout of project

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index)=> {
+            const Page = route.component;
+            const Layout = route.layout === null ? Fragment : DefaultLayout || DefaultLayout;
+            return (
+            <Route 
+              key={index} 
+              path={route.path} 
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+            }/>)
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
