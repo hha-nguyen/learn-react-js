cf28db23ceb89f8cc386883e65b68e46dc4dcf66
Mesto <xamlol2982002@gmail.com>
2022-08-12 23:23:28 +0700

    start making cursor of sidebar

--- tiktok/src/layouts/components/Sidebar/Sidebar.js
+++ tiktok/src/layouts/components/Sidebar/Sidebar.js
@@ -15,6 +15,7 @@ import Suggest from './Suggest';
 import { GROUP_OF_USER_DATA } from './Suggest';
 import Discover from './Discover/Discover';
 import Footer from './Footer/Footer';
+import Scrollbar from './Scrollbar';
 
 const cx = classNames.bind(styles);
 
@@ -42,35 +43,39 @@ const getSuggestUsers = (data) => {
 
 function Sidebar() {
     return (
-        <aside className={cx('wrapper')}>
-            <Menu>
-                <MenuItem
-                    title="For You"
-                    to={config.routes.home}
-                    icon={<HomeIcon />}
-                    activeIcon={<HomeActiveIcon />}
-                ></MenuItem>
-                <MenuItem
-                    title="Following"
-                    to={config.routes.following}
-                    icon={<UserGroupIcon />}
-                    activeIcon={<UserGroupActiveIcon />}
-                ></MenuItem>
-                <MenuItem
-                    title="LIVE"
-                    to={config.routes.live}
-                    icon={<LiveIcon />}
-                    activeIcon={<LiveActiveIcon />}
-                ></MenuItem>
-            </Menu>
+        <aside className={cx('containter')}>
+            <div className={cx('wrapper')}>
+                <Menu>
+                    <MenuItem
+                        title="For You"
+                        to={config.routes.home}
+                        icon={<HomeIcon />}
+                        activeIcon={<HomeActiveIcon />}
+                    ></MenuItem>
+                    <MenuItem
+                        title="Following"
+                        to={config.routes.following}
+                        icon={<UserGroupIcon />}
+                        activeIcon={<UserGroupActiveIcon />}
+                    ></MenuItem>
+                    <MenuItem
+                        title="LIVE"
+                        to={config.routes.live}
+                        icon={<LiveIcon />}
+                        activeIcon={<LiveActiveIcon />}
+                    ></MenuItem>
+                </Menu>
 
-            <Suggest title={"Suggested accounts"} data={getSuggestUsers(GROUP_OF_USER_DATA)}/> 
+                <Suggest title={"Suggested accounts"} data={getSuggestUsers(GROUP_OF_USER_DATA)}/> 
 
-            <Suggest title={"Following accounts"} data={getFollowedUsers(GROUP_OF_USER_DATA)}/>
-            
-            <Discover/> 
+                <Suggest title={"Following accounts"} data={getFollowedUsers(GROUP_OF_USER_DATA)}/>
+                
+                <Discover/> 
 
-            <Footer />
+                <Footer />
+            </div>
+        
+            <Scrollbar />
         </aside>
     );
 }
--- tiktok/src/routes/routes.js
+++ tiktok/src/routes/routes.js
@@ -7,7 +7,6 @@ import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/layouts/components/Search/Search';
 import Live from '~/pages/Live';
-import Discover from '~/layouts/components/Sidebar/Discover';
 import DefaultLayout from '~/layouts';
 
 const publicRoutes = [
@@ -17,7 +16,7 @@ const publicRoutes = [
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: null},
     { path: config.routes.search, component: Search, layout: null},
-    { path: config.routes.tag, component: Discover, layout: DefaultLayout},
+    { path: config.routes.tag, component: Home, layout: DefaultLayout},
 ];
 
 const privateRoutes = [
